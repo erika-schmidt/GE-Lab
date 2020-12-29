63272047356d55a008e97008b2e3024490df1bb3
Sharu <72849546+CanCruz@users.noreply.github.com>
2020-12-29 08:40:24 +0100

    test

---
+++ Projekt_fkk_Corona/Assets/CHARACTER/Animation/Jump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public Animator animator;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the animator
+
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Inputspace.GetButtonDown("Jump")) //wert erhöhen
+        {
+            animator.SetTrigger("Space");
+        }
+    }
+}
